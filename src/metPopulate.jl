a3565d4fc6e00f5695aeb4be8bf465fc04916537
Caroline <carolinedevoto@Carolines-MacBook-Pro-2.local>
2020-11-15 19:38:02 -0500

    initial metPopulate for taxa and taxa_seq_id, will go through tomorrow

--- src/metPopulate.jl
+++ src/metPopulate.jl
@@ -1,53 +1,68 @@
+module metPopulate
+
 using ArgParse
 using LibPQ
+using FASTX
 
-# to run: Julia met-populate.jl -n silva -l "https://ftp.arb-silva.de/release_100/ARB_files/SSUParc_100_SILVA_02_08_09_opt.arb"
+include("API.jl")
+using .API_calls
+
+# to run: Julia pop.jl -s silva -p "/Users/carolinedevoto/Downloads/SILVA_138.1_SSU_tax_silva_trunc.fasta"
+# to run: Julia pop.jl -s silva -p "/Users/carolinedevoto/test_silva.fasta"
 # have to figure out how to add as bin script
 
 function parse_commandline()
-	s = ArgParseSettings()
+	settings = ArgParseSettings()
 
-	@add_arg_table s begin
-		"--name", "-n"
-			help = "name of database to be inserted"
+	@add_arg_table settings begin
+		"--source", "-s"
+			help = "name of source to be inserted, ex. SILVA"
 			arg_type = String
 			required = true
-		"--link", "-l"
-			help = "link to database"
+		"--path", "-p"
+			help = "path to fasta file"
 			required = true
-		"--overwrite", "-r"
-            help = "option to overwrite existing table (true or false)"
-            default = false
 	end
 
-	return parse_args(s)
+	return parse_args(settings)
 end
 
-# insertDB just connects to database currently, need to figure out postgresql command to insert from link
-# check to see if table exists, if overwrite = true then overwrite, if false give error message
-# if table doesnt exist insert as normal
+function fasta_insert(source, path)
+	reader = open(FASTA.Reader, path)
+	for record in reader
+		# split description based on delimiter (domain-kingdom-phylum-class-order-family-genus-species)
+		lst = rsplit(FASTA.description(record), ";")
+		inserted = false
+		try
+			# indexing represents order, family, genus, species
+			API_calls.insert_API_taxa(lst[4], lst[5], lst[6], lst[7])
+			inserted = true
+		catch e
+			println("order, family, genus, species not all defined")
+		end
 
-function insertDB(name, link, overwrite)
-	host="127.0.0.1"
-	port="5432"
-	db="met"
-	user="cdevoto_nopass"
+		# only insert into taxa_seq_id if insertion into taxa was successful
 
-	conn = LibPQ.Connection("host=$host dbname=$db user=$user password=$pwd")
+		if inserted == true
+			# API call to get taxon_id from taxa table, need it to insert into taxa_seq_id
+			resp = API_calls.get_API_taxa(lst[4], lst[5], lst[6], lst[7])
+			id = replace(resp, "[" => "")
+			id = replace(id, "]" => "")
 
-	result = LibPQ.execute(conn, "CREATE TABLE $name (test varchar(10) PRIMARY KEY);")
+			API_calls.insert_API_seqID(parse(Int64, id), FASTA.sequence(record), source, FASTA.identifier(record))
+		else
+			println("error inserting in taxa, unable to insert in taxa_seq_id")
+		end
 
-
+	end
+	close(reader)
 end
 
 function main()
-	parsed_args = parse_commandline()
-	
-	insertDB(parsed_args["name"], parsed_args["link"], parsed_args["overwrite"])
-	println("Parsed args:")
-	for (arg,val) in parsed_args
-        println("  $arg  =>  $val")
-	end
+	parsed_args = parse_commandline()	
+	fasta_insert(parsed_args["source"], parsed_args["path"])
 end
 
 main()
+
+end
