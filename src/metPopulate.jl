252022176db138d52c5cc11861ad981b9a2e6917
dave <dave@d3fy.net>
2021-02-18 10:16:01 -0500

    yeah, that wouldn't work metAPIcalls not API_calls

--- src/metPopulate.jl
+++ src/metPopulate.jl
@@ -23,7 +23,7 @@ function fasta_insert(source, path)
         inserted = false
         try
             # indexing represents order, family, genus, species
-            API_calls.insert_API_taxa(lst[4], lst[5], lst[6], lst[7])
+            metAPIcalls.insert_API_taxa(lst[4], lst[5], lst[6], lst[7])
             inserted = true
         catch e
             println("order, family, genus, species not all defined")
@@ -33,11 +33,11 @@ function fasta_insert(source, path)
 
         if inserted == true
             # API call to get taxon_id from taxa table, need it to insert into taxa_seq_id
-            resp = API_calls.get_API_taxa(lst[4], lst[5], lst[6], lst[7])
+            resp = metAPIcalls.get_API_taxa(lst[4], lst[5], lst[6], lst[7])
             id = replace(resp, "[" => "")
             id = replace(id, "]" => "")
 
-            API_calls.insert_API_seqID(parse(Int64, id), FASTA.sequence(record), source, FASTA.identifier(record))
+            metAPIcalls.insert_API_seqID(parse(Int64, id), FASTA.sequence(record), source, FASTA.identifier(record))
         else
             println("error inserting in taxa, unable to insert in taxa_seq_id")
         end
@@ -59,17 +59,17 @@ function SraRunInfo_insert(local_path, download_path)
 
 	# See if project identifier exists already in table, otherwise insert 
 	
-	project_id = API_calls.get_API_projects(data_df.Experiment[1])
+	project_id = metAPIcalls.get_API_projects(data_df.Experiment[1])
 	
 	if project_id == "null"
 		println("inserting experiment into table projects ... ")
-		API_calls.insert_API_projects(data_df.Experiment[1], data_df.Experiment[1], data_df.BioProject[1])
+		metAPIcalls.insert_API_projects(data_df.Experiment[1], data_df.Experiment[1], data_df.BioProject[1])
 	end
 
 	# Insert run into dataset table
 
 	try
-		API_calls.insert_API_dataset(data_df.Run[1], data_df.Run[1], download_path)
+		metAPIcalls.insert_API_dataset(data_df.Run[1], data_df.Run[1], download_path)
 	catch e
 		println("error inserting into dataset table. Does run $data_df.Run[1] already exist in table?")
 	end
@@ -99,17 +99,17 @@ function SraRunInfo_insert(local_path, download_path)
 
 	# Retrieve dataset_id from dataset table
 	
-	id = API_calls.get_API_dataset(data_df.Run[1])
+	id = metAPIcalls.get_API_dataset(data_df.Run[1])
 	dataset_id = replace(id, "[" => "")
     dataset_id = replace(dataset_id, "]" => "")
 
 	# Add dataset_id to project_id table
 
-	API_calls.update_API_projects(parse(Int64, dataset_id), data_df.Experiment[1])	
+	metAPIcalls.update_API_projects(parse(Int64, dataset_id), data_df.Experiment[1])	
 
 	# Retrieve project_id from project table	
 
-	project_id = API_calls.get_API_projects(data_df.Experiment[1])
+	project_id = metAPIcalls.get_API_projects(data_df.Experiment[1])
 	project_id = replace(project_id, "[" => "")
     project_id = replace(project_id, "]" => "")
 
@@ -149,7 +149,7 @@ function SraRunInfo_insert(local_path, download_path)
 	sequencing_platform = data_df.Platform[1]
     release_date = data_df.ReleaseDate[1]
 
-	API_calls.insert_API_datasetmetadata(dataset_id, dataset_external_identifier, project_id, collection_identifier, experiment_identifier, sample_name, study_identifier, assay_type, AvgSpotLen, publisher, collection_date, sample_depth, sample_elev, sample_sal, env_biome, env_feature, env_material, geo_loc_name_country, geo_loc_name_country_continent, geo_loc_name, loc_type, sequencing_instrument, isolation_source, lat_lon, library_name, library_layout, library_selection, library_source[1], MBases, MBytes, sequencing_platform, release_date)
+	metAPIcalls.insert_API_datasetmetadata(dataset_id, dataset_external_identifier, project_id, collection_identifier, experiment_identifier, sample_name, study_identifier, assay_type, AvgSpotLen, publisher, collection_date, sample_depth, sample_elev, sample_sal, env_biome, env_feature, env_material, geo_loc_name_country, geo_loc_name_country_continent, geo_loc_name, loc_type, sequencing_instrument, isolation_source, lat_lon, library_name, library_layout, library_selection, library_source[1], MBases, MBytes, sequencing_platform, release_date)
 
 end
 
@@ -178,7 +178,7 @@ function SraFASTQ_insert(sra_num, gene_region)
     fastq_filepath = joinpath(fastq_folder, string(sra_num, ".fastq"))
 
     ## dataset id will be the same for all (same sra number)
-    dataset_id = API_calls.get_API_dataset(sra_num)
+    dataset_id = metAPIcalls.get_API_dataset(sra_num)
     dataset_id = replace(dataset_id, "[" => "")
     dataset_id = replace(dataset_id, "]" => "")
 
@@ -224,18 +224,18 @@ function SraFASTQ_insert(sra_num, gene_region)
     for seq in keys(seq_dict)
         try
             ## currently quality_score is type text in asv table
-            API_calls.insert_API_asv(seq, string(floor(seq_dict[seq][1])), gene_region)
+            metAPIcalls.insert_API_asv(seq, string(floor(seq_dict[seq][1])), gene_region)
         catch e
             println("error inserting into asv table. is the sequence unique to the table?")
             inserted = 1
         end
 
         if inserted == 0
-            asv_id = API_calls.get_API_asv(seq)
+            asv_id = metAPIcalls.get_API_asv(seq)
             asv_id = replace(asv_id, "[" => "")
             asv_id = replace(asv_id, "]" => "")
 
-            API_calls.insert_API_asvAssignment(parse(Int64, asv_id), parse(Int64, dataset_id), convert(Int64, seq_dict[seq][2]))
+            metAPIcalls.insert_API_asvAssignment(parse(Int64, asv_id), parse(Int64, dataset_id), convert(Int64, seq_dict[seq][2]))
         end
     end
 
