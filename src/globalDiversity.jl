c7178262080b908f603dd46764de98be20230a3e
dave <dave@d3fy.net>
2021-02-19 14:57:48 -0500

    added calls to dataset_taxa_table

--- src/globalDiversity.jl
+++ src/globalDiversity.jl
@@ -2,4 +2,8 @@ module globalDiversity
 
 using SpatialEcology;
 
+# The point of this function is to develop a table of: Dataset_id, Lat, Long, # Different ASV, environment type
+#function constructDiversity()
+
+
 end
