522d3a725ba9f5399a2a289c64cfa49ba82b642f
dave <dave@d3fy.net>
2021-02-27 12:39:00 -0500

    quick updates

--- src/globalDiversity.jl
+++ src/globalDiversity.jl
@@ -2,8 +2,20 @@ module globalDiversity
 
 using SpatialEcology;
 
+include("metAPIcalls.jl")
+using .metAPIcalls
+
+
 # The point of this function is to develop a table of: Dataset_id, Lat, Long, # Different ASV, environment type
-#function constructDiversity()
+# Mat could you take a look at this?
+function plotDiversity()
+	datasets = .metAPIcalls.get_API_datasets_alpha()
+	#Dataframes
+	#amph = Assemblage()
+end
 
-
+function plotAssemblage()
+	
+	
+	
 end
